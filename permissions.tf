455b0246fa88eab76dba96c7e2b94f8a7234209d
kumari-shubham <92294410+kumari-shubham@users.noreply.github.com>
2024-01-11 15:06:41 +0530

    Initial commit

---
+++ permissions.tf
@@ -0,0 +1,100 @@
+resource "aws_iam_role" "byos_object_permissions" {
+  name = "firefliesai_aws_byos_role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Effect = "Allow",
+        Principal = {
+          AWS = [
+          "arn:aws:iam::466023587921:root",
+          "arn:aws:iam::466023587921:user/rtmp-server-user-0",
+           ],
+        },
+        Action = "sts:AssumeRole",
+      },
+    ],
+  })
+}
+
+resource "aws_iam_policy" "byos_object_permissions_policy" {
+  name        = "FirefliesAI_BYOS_Object_Permissions"
+  description = "Fireflies AI BYOS Object Permissions Policy"
+
+  policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Effect   = "Allow",
+        Action   = [
+          "s3:PutObject",
+          "s3:DeleteObject",
+          "s3:GetObject",
+          "s3:GetObjectAcl",
+          "s3:ListBucket",
+          "s3:PutObjectAcl",
+        ],
+        Resource = [
+          "arn:aws:s3:::${var.bucket_name}/*",
+        ],
+      },
+    ],
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "byos_object_permissions_attachment" {
+  policy_arn = aws_iam_policy.byos_object_permissions_policy.arn
+  role       = aws_iam_role.byos_object_permissions.name
+}
+
+resource "aws_iam_role" "bucket_permissions" {
+  name = "firefliesai_aws_byos_bucket_role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Effect = "Allow",
+        Principal = {
+          AWS = [
+          "arn:aws:iam::466023587921:root",
+          "arn:aws:iam::466023587921:user/rtmp-server-user-0",
+         ],
+        },
+        Action = "sts:AssumeRole",
+      },
+    ],
+  })
+}
+
+resource "aws_iam_policy" "bucket_permissions_policy" {
+  name        = "FirefliesAI_BYOS_Bucket_Permissions"
+  description = "Fireflies AI BYOS Bucket Permissions Policy"
+
+  policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Effect   = "Allow",
+        Action   = [
+          "s3:ListBucket",
+          "s3:GetBucketAcl",
+          "s3:GetBucketPolicy",
+        ],
+        Resource = [
+          "arn:aws:s3:::${var.bucket_name}",
+        ],
+      },
+    ],
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "bucket_permissions_attachment" {
+  policy_arn = aws_iam_policy.bucket_permissions_policy.arn
+  role       = aws_iam_role.bucket_permissions.name
+}
+
+output "role_arn" {
+  value = aws_iam_role.byos_object_permissions.arn
+}
---
+++ variables.tf
@@ -0,0 +1,9 @@
+variable "bucket_name" {
+  type = string
+}
+variable "access_key" {
+  type = string
+}
+variable "secret_key" {
+  type = string
+}
